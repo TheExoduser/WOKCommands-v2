5bb8e64cb6874db9a3db71dd9713658ba16021cb
Christian Holzwarth <christian@holzwarth.family>
2023-07-28 01:04:27 +0200

    Remove commonjs exports

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import WOK from "./WOK.js";
 import CommandType from "./util/CommandType.js";
 import CooldownTypes from "./util/CooldownTypes.js";
 import DefaultCommands from "./util/DefaultCommands.js";
-
+/*
 module.exports = WOK;
 module.exports.CommandType = CommandType;
 module.exports.CooldownTypes = CooldownTypes;
 module.exports.DefaultCommands = DefaultCommands;
-
+*/
 export default WOK;
 export { CommandType, CooldownTypes, DefaultCommands };
