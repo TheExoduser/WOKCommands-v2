25048b615716aed63dbb308e77675973e4f81642
Christian Holzwarth <christian@holzwarth.family>
2023-08-01 15:53:06 +0000

    Compiled ts

--- dist/event-handler/events/interactionCreate/isAutocomplete/autocomplete.js
+++ dist/event-handler/events/interactionCreate/isAutocomplete/autocomplete.js
@@ -13,7 +13,7 @@ export default async (interaction, client, instance) => {
         return;
     }
     const focusedOption = interaction.options.getFocused(true);
-    const choices = await autocomplete(command, focusedOption.name, interaction);
+    const choices = await autocomplete(command, focusedOption.name, interaction, client, instance);
     const filtered = choices
         .filter((choice) => {
         const choiceName = choice.name || choice;
